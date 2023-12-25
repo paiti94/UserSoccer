a5d74e167aa10e856c8c2ea477ba9e2fde80ce82
paiti94 <paiti94@gmail.com>
2023-12-25 15:05:05 -0800

    Update Athlete controller

--- UseSoccer/src/main/java/com/human/springboot/AthleteController/AthleteController.java
+++ UseSoccer/src/main/java/com/human/springboot/AthleteController/AthleteController.java
@@ -1,10 +1,13 @@
 package com.human.springboot.AthleteController;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.google.gson.Gson;
@@ -27,7 +30,6 @@ public class AthleteController {
     				
     	}
 	}
-	
 	@GetMapping("/athleteall")
 	public String getPlayerAll() {
 		Gson gson = new Gson();
@@ -37,6 +39,30 @@ public class AthleteController {
     		return gson.toJson("error");
     				
     	}
+	}
+	@GetMapping("/athleteallbypage")
+	public String getPlayerAllByPage(@RequestParam(defaultValue = "1") int page,
+		    @RequestParam(defaultValue = "10") int itemsPerPage) {
+		Gson gson = new Gson();
+//    	if(athleteService.getPlayers() != null) {
+//    		return gson.toJson(athleteService.getPlayers());
+//    	}else {
+//    		return gson.toJson("error");
+//    				
+//    	}
+		 List<AthleteDTO> players = athleteService.getPlayers();
+
+		    if (players != null) {
+		        // Implement pagination logic here based on the provided page and itemsPerPage
+		        int startIndex = (page - 1) * itemsPerPage;
+		        int endIndex = Math.min(startIndex + itemsPerPage, players.size());
+
+		        List<AthleteDTO> paginatedPlayers = players.subList(startIndex, endIndex);
+
+		        return gson.toJson(paginatedPlayers);
+		    } else {
+		        return gson.toJson("error");
+		    }
 	}
 	
 	@GetMapping("/athlete/match/{round}")
